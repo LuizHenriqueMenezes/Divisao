cb5488e8e8bc0fb6cf32a6d1d213dd388522c673
Luiz Henrique <lmenezesmessias@gmail.com>
2021-04-10 16:35:13 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos resolver sua conta de divisão, para isso:!");
+
+            Console.Write("Digite o numerador:");
+            double numerador = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.Write("Digite o denominador:");
+            double denominador = Convert.ToDouble(Console.ReadLine()); 
+
+            double resultado = numerador / denominador; 
+
+            if (denominador == 0) 
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+            }
+            else
+            {
+                Console.WriteLine($"{numerador} dividido por {denominador} é {resultado}");
+            }
+        }
+    }
+}
